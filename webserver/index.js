85eb3a38e988e42f659066aa130de5b148ee154d
phynxinferno <phynxinferno1@gmail.com>
2022-02-03 18:43:30 -0500

    https is hard

--- webserver/index.js
+++ webserver/index.js
@@ -1,23 +1,17 @@
-var http = require('http');
+var https = require('https');
 var Discord = require('discord.js');
 var url = require('url');
 var properties = require('./properties.json')
-console.log(__dirname)
-var fs = require('fs').promises;
-const listener = function (req, res) {
-    var site = url.parse(req.url).pathname;
-    switch(path) {
-        case '/' :
-            fs.readFile(__dirname + '/pages/home/index.html')
-    .then(contents => {
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(contents);
-    });
-        
-    }
+var fs = require('fs');
+const options = {
+    key: fs.readFileSync('path'),
+    cert: fs.readFileSync('path')
 }
-const server = http.createServer(listener);
-server.listen(properties.Port,'127.0.0.1', () => {
-    //e
-})
+var server = https.createServer(options, function(req, res){
+    console.log(req.readable);
+    //res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    //res.end(fs.readFileSync(__dirname + "/pages/home/index.html"))
+    res.end('among us');
+}).listen(properties.Port,properties.Hostname);
+console.log("Webserver started. Listening at " + properties.Hostname + " on port " + properties.Port + ' and showing HTML file ' + __dirname + "/pages/home/index.html");
