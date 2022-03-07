58d2ac3f341f2f4800ee7c8e10299ab5ed05692c
phynxinferno <phynxinferno1@gmail.com>
2022-03-07 15:04:16 -0500

    added node modules to the gitignore

--- webserver/index.js
+++ webserver/index.js
@@ -1,17 +1,15 @@
 var https = require('https');
 var Discord = require('discord.js');
-var url = require('url');
 var properties = require('./properties.json')
 var fs = require('fs');
 const options = {
-    key: fs.readFileSync('path'),
-    cert: fs.readFileSync('path')
+    key: fs.readFileSync('/home/phynx/SchooLFG-Keys/key.pem'),
+    cert: fs.readFileSync('/home/phynx/SchooLFG-Keys/cert.pem')
 }
 var server = https.createServer(options, function(req, res){
-    console.log(req.readable);
-    //res.setHeader("Content-Type", "text/html");
+    console.log(req.headers);
+    res.setHeader("Content-Type", "text/html");
     res.writeHead(200);
-    //res.end(fs.readFileSync(__dirname + "/pages/home/index.html"))
-    res.end('among us');
+    res.end(fs.readFileSync(__dirname + "/pages/home/index.html"))
 }).listen(properties.Port,properties.Hostname);
 console.log("Webserver started. Listening at " + properties.Hostname + " on port " + properties.Port + ' and showing HTML file ' + __dirname + "/pages/home/index.html");
